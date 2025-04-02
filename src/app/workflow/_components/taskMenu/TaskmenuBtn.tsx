c5a10554fd7dafd62e7d9f8e62ba750011a96d89
Rishab raj <rishabraj2211@gmail.com>
2025-04-02 13:13:39 +0530

    improved taskmenu ui

--- src/app/workflow/_components/taskMenu/TaskmenuBtn.tsx
+++ src/app/workflow/_components/taskMenu/TaskmenuBtn.tsx
@@ -1,6 +1,9 @@
 import { TaskType } from "@/app/types/tasks";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { TaskRegistry } from "@/lib/workflow/task/Registry";
+import { CoinsIcon } from "lucide-react";
+
 
 export function TaskmenuBtn({taskType} : {taskType : TaskType}){    
     const task = TaskRegistry[taskType];
@@ -10,8 +13,14 @@ export function TaskmenuBtn({taskType} : {taskType : TaskType}){
         event.dataTransfer.effectAllowed = "move";
     }
 
-    return  (<Button variant={"outline"} draggable onDragStart={(event)=> onDragStartEvent(event, taskType)} >
+    return  (<Button variant={"outline"} draggable onDragStart={(event)=> onDragStartEvent(event, taskType)} className="flex justify-between" >
+        <div className="flex gap-2 items-center">
         <task.icon size={20}/>
         {task.label}
+        </div>
+        <Badge variant={"outline"} className="flex gap-1">  
+            <CoinsIcon className="text-yellow-500"/>
+            {task.credits}
+        </Badge>
     </Button>)
 }
